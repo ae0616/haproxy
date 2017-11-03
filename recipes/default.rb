d3bbb9b3f2dd956a523c32d45cc8b31cf95beeb6
Andrew Engelhard <andy.engelahrd@gmail.com>
2017-11-03 15:18:02 +0000

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+node.default['haproxy']['members'] = [{
+	'hostname' => 'ec2-54-163-116-239.compute-1.amazonaws.com',
+	'ipaddress' => '54.163.116.239',
+	'port' => 80,
+	'ssl_port' => 80
+},{
+	'hostname' => 'ec2-52-90-218-161.compute-1.amazonaws.com',
+	'ipaddress' => '52.90.218.161',
+	'port' => 80,
+	'ssl_port' => 80
+}]
+
+include_recipe	'haproxy::manual'
