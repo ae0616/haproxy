6904865108fcb5960d509c0bff80cb545cb0fcb9
Andrew Engelhard <andy.engelahrd@gmail.com>
2017-11-03 18:48:01 +0000

    0.2.1, use dynamic role template instead of hardcoded webserver

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,17 +4,33 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+all_web_nodes = search('node', 'role:web_server')
 
-node.default['haproxy']['members'] = [{
-	'hostname' => 'ec2-54-163-116-239.compute-1.amazonaws.com',
-	'ipaddress' => '54.163.116.239',
-	'port' => 80,
-	'ssl_port' => 80
-},{
-	'hostname' => 'ec2-52-90-218-161.compute-1.amazonaws.com',
-	'ipaddress' => '52.90.218.161',
-	'port' => 80,
-	'ssl_port' => 80
-}]
+members = []
+all_web_nodes.each do |web_node|
+	member = {
+		'hostname' => web_node['cloud']['public_hostname'],
+		'ipaddress' => web_node['cloud']['public_ipv4'],
+		'port' => 80,
+		'ssl_port' => 80
+	}
+	members.push(member)
+end
+
+node.default['haproxy']['members'] = members
+
+#node.default['haproxy']['members'] = [{
+	#'hostname' => 'ec2-54-163-116-239.compute-1.amazonaws.com',
+	#'ipaddress' => '54.163.116.239',
+	#'port' => 80,
+	#'ssl_port' => 80
+#},{
+	#'hostname' => 'ec2-52-90-218-161.compute-1.amazonaws.com',
+	#'ipaddress' => '52.90.218.161',
+	#'port' => 80,
+	#'ssl_port' => 80
+#}]
+
+
 
 include_recipe	'haproxy::manual'
